180955b0e4ac062e565afa3fef3654169bfca188
Matias Camiletti <matias.camiletti@gmail.com>
2023-06-20 15:57:22 -0300

    Add migration.

---
+++ database/migrations/2023_06_20_152209_create_tots_user_code_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tots_user_code', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('sent', 250)->nullable(true);
+            $table->string('code', 10)->index()->nullable(false);
+            $table->tinyInteger('provider')->nullable(false)->default(0)->comment('0 = Email');
+            $table->tinyInteger('status')->nullable(false)->default(0)->comments('0 = Pending, 1 = Used, 2 = Expired');
+            $table->dateTime('expired_at')->nullable(true);
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('tots_user');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tots_user_code');
+    }
+};
