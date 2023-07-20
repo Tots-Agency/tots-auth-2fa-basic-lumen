1b6dce5eaa62beb9512c24aefdb46ad3336ab326
Matias Camiletti <matias.camiletti@gmail.com>
2023-07-20 17:31:14 -0300

    Add email library.

--- src/Http/Controllers/UserCodeController.php
+++ src/Http/Controllers/UserCodeController.php
@@ -7,21 +7,31 @@ use Tots\AuthTfaBasic\Http\Requests\TfaBasicRequest;
 use Tots\AuthTfaBasic\Http\Responses\TfaBasicResponse;
 use Tots\AuthTfaBasic\Models\TotsUserCode;
 use Tots\AuthTfaBasic\Services\UserCodeService;
+use Tots\Email\Services\TotsEmailService;
 
 class UserCodeController extends \Laravel\Lumen\Routing\Controller
 {
     protected UserCodeService $service;
+    protected TotsEmailService $emailService;
 
-    public function __construct(UserCodeService $service)
+    public function __construct(UserCodeService $service, TotsEmailService $emailService)
     {
         $this->service = $service;
+        $this->emailService = $emailService;
     }
 
     public function send(Request $request)
     {
         // Validation
         $this->validate($request, TfaBasicRequest::rules());
+        // Generate new code
+        $code = $this->service->create($request->input('email'), TotsUserCode::PROVIDER_EMAIL);
+        // Send email
+        $this->emailService->send($request->input('email'), 'recover-password-code', [
+            'code' => $code->code,
+            'expired_at' => $code->expired_at
+        ]);
         // Reponse
-        return TfaBasicResponse::make($this->service->create($request->input('email'), TotsUserCode::PROVIDER_EMAIL));
+        return TfaBasicResponse::make($code);
     }
 }
